84684ced9180052ca463dccf45ee7dab485ce0f2
sahilmazmudar <sahil.maz97@gmail.com>
2022-03-27 12:34:56 -0700

    fix: Typo

--- js/haikyu-loader.js
+++ js/haikyu-loader.js
@@ -1,6 +1,6 @@
 var haikyus = [
-  ["3/27/22","A fresh powder day", "A tad faster than yesterday", "'Pop' goes the tailbone."],
-	["3/26/22","Words floating around", "Pick and chose  desire", "Soon, there will be none."],
+  ["3/27/22","A fresh powder day", "Tad faster than yesterday", "'Pop' goes the tailbone."],
+	["3/26/22","Words floating around", "Pick and chose what you desire", "Soon, there will be none."],
   ["3/25/22","$500 lost again", "'I swear the next one is it'", "Make another bet."],
 ]
 
